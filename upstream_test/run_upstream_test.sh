b61f20d9a47f9fe1181afd0e7f2221dc0568adf7
Balamuruhan S <bala24@linux.vnet.ibm.com>
2019-02-23 19:57:39 +0530

    minor fix

--- upstream_test/run_upstream_test.sh
+++ upstream_test/run_upstream_test.sh
@@ -1,26 +1,24 @@
 # !/usr/bin/sh
 
-mkdir -p /var/tmp/upstream_test
-cd /var/tmp/upstream_test
 git clone https://github.com/open-power/op-test-framework.git
-cd ./op-test-framework
+cd /tmp/upstream/op-test-framework
 
-rm -f machine_host.conf
+rm -f $1.conf
 
 # install upstream kernel in the host
-wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/machine_configs/$1" -O machine_host.conf
-./op-test -c machine_host.conf --run testcases.InstallUpstreamKernel.InstallUpstreamKernel
+# wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/machine_configs/$1.conf" -O machine_host.conf
+/tmp/upstream/op-test-framework/op-test -c /tmp/upstream/op-test-framework/upstream_test/machine_configs/$1.conf --run testcases.InstallUpstreamKernel.InstallUpstreamKernel
 
 # install required packages, build libirt, qemu in host, build kernel for guest
-wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_env.cmd" -O env.cmd
-./op-test -c machine_test.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file env.cmd
+# wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_env.cmd" -O env.cmd
+/tmp/upstream/op-test-framework/op-test -c /tmp/upstream/op-test-framework/upstream_test/machine_configs/$1.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file /tmp/upstream/op-test-framework/upstream_test/test_configs/host_cmd_upstream_env.cmd
 
 # run localhost migration test with upstream libvirt qemu and kernel
-wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_localhost_migration.cmd" -O migration.cmd
-./op-test -c machine_test.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file migration.cmd
+#wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_localhost_migration.cmd" -O migration.cmd
+/tmp/upstream/op-test-framework/op-test -c /tmp/upstream/op-test-framework/upstream_test/machine_configs/$1.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file /tmp/upstream/op-test-framework/upstream_test/test_configs/host_cmd_upstream_localhost_migration.cmd
 
-wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_lifecycle.cmd" -O lifecycle.cmd
-./op-test -c machine_test.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file lifecycle.cmd
+#wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_lifecycle.cmd" -O lifecycle.cmd
+/tmp/upstream/op-test-framework/op-test -c /tmp/upstream/op-test-framework/upstream_test/machine_configs/$1.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file /tmp/upstream/op-test-framework/upstream_test/test_configs/host_cmd_upstream_lifecycle.cmd
 
-wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_lifecycle.cmd" -O lifecycle.cmd
-./op-test -c machine_test.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file lifecycle.cmd
+#wget "https://raw.githubusercontent.com/balamuruhans/op-test-framework/upstream_test/upstream_test/test_configs/host_cmd_upstream_lifecycle.cmd" -O lifecycle.cmd
+/tmp/upstream/op-test-framework/op-test -c /tmp/upstream/op-test-framework/upstream_test/machine_configs/$1.conf --run testcases.RunHostTest.RunHostTest --host-cmd-file /tmp/upstream/op-test-framework/upstream_test/test_configs/host_cmd_upstream_lifecycle.cmd
